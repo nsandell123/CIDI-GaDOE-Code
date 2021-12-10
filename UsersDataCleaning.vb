1c7c507a5f52eaf3344ab53c4031f3b60807a066
Neel Sandell <sandellfamily2013@gmail.com>
2021-12-10 01:55:12 -0500

    combined for loops and added fully qualified names

--- UsersDataCleaning.vb
+++ UsersDataCleaning.vb
@@ -1,18 +1,14 @@
 Sub UsersDataCleaning()
-    'Deleting Matthew Blake and Andrew Gelinas'
-    For i = Cells(Rows.Count, 1).End(xlUp).Row To 1 Step -1
-    If Cells(i, 1).Value = "Matthew Blake" Or Cells(i, 1).Value = "Andrew  Gelinas" Then
-            Cells(i, 1).EntireRow.Delete
+    Worksheets(sheetNames(4)).Select
+    For i = Worksheets(sheetNames(4)).Cells(Rows.Count, 1).End(xlUp).Row To 1 Step -1
+        'Deleting Matthew Blake and Andrew Gelinas'
+        If Worksheets(sheetNames(4)).Cells(i, 1).Value = "Matthew Blake" Or Worksheets(sheetNames(4)).Cells(i, 1).Value = "Andrew  Gelinas" Then
+            Worksheets(sheetNames(4)).Cells(i, 1).EntireRow.Delete
+        End If
+        'Populating Oconee GLRS on 9 - Oconee'
+        If Worksheets(sheetNames(4)).Cells(i, 6).Value = "9 - Oconee" And Worksheets(sheetNames(4)).Cells(i, 7).Value = "" Then
+            Worksheets(sheetNames(4)).Cells(i, 7).Value = "Oconee GLRS"
         End If
     Next i
-
-    'Populating Oconee GLRS on 9 - Oconee'
-
-
- For i = Cells(Rows.Count, 1).End(xlUp).Row To 1 Step -1
-    If Cells(i, 6).Value = "9 - Oconee" And Cells(i, 7).Value = "" Then
-    Cells(i, 7).Value = "Oconee GLRS"
-    End If
-    Next i
-
+    
 End Sub
