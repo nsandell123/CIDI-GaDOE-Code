4f1a6f45d6e0ef296793fb0ac22a5e8cb9bba863
Neel Sandell <sandellfamily2013@gmail.com>
2021-12-10 01:36:15 -0500

    Create GlobalChecking.vb

---
+++ GlobalChecking.vb
@@ -0,0 +1,12 @@
+Option Explicit On
+Global sheetNames(12) As String
+Global mainWorkbook As Workbook
+Sub GlobalChecking()
+    Set mainWorkbook = ActiveWorkbook
+    Dim i As Long
+    For i = 1 To 12
+        sheetNames(i) = mainWorkbook.Sheets(i).Name
+    Next i
+
+End Sub
+
