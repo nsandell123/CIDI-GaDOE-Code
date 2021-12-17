73c9c5b3e5177b8b9cd7990999709bbec1e19667
Neel Sandell <sandellfamily2013@gmail.com>
2021-12-17 03:44:32 -0500

    Formatted Date/Time from raw csv

--- FormattingBoldItalics.vb
+++ FormattingBoldItalics.vb
@@ -4,7 +4,18 @@ Sub RequestsFormatting()
 '
 
 '
-
+    Dim LRowFirst As Long
+    LRowFirst = Worksheets(sheetNames(2)).Cells(Rows.Count, "C").End(xlUp).Row
+    Dim Rng1 As Range
+    Set Rng1 = Worksheets(sheetNames(2)).Range("C1:" + "C" + Right(Str(LRowFirst), Len(LRowFirst) - 1))
+    
+    Dim cellplaceholder As Range
+    
+    For Each cellplaceholder In Rng1
+        cellplaceholder.NumberFormat = "m/d/yyyy"
+        cellplaceholder.Value = cellplaceholder.Value
+    Next cellplaceholder
+    
     Worksheets(sheetNames(2)).Select
     Worksheets(sheetNames(2)).Rows("1:1").Select
     Selection.Font.Bold = True
@@ -17,8 +28,20 @@ Sub UsersFormatting()
 '
 
 '
-    Worksheets(sheetNames(5)).Select
-    Worksheets(sheetNames(5)).Rows("1:1").Select
+    Dim LRowFirst As Long
+    LRowFirst = Worksheets(sheetNames(4)).Cells(Rows.Count, "D").End(xlUp).Row
+    Dim Rng1 As Range
+    Set Rng1 = Worksheets(sheetNames(4)).Range("D1:" + "D" + Right(Str(LRowFirst), Len(LRowFirst) - 1))
+    
+    Dim cellplaceholder As Range
+    
+    For Each cellplaceholder In Rng1
+        cellplaceholder.NumberFormat = "m/d/yyyy h:mm"
+        cellplaceholder.Value = cellplaceholder.Value
+    Next cellplaceholder
+    
+    Worksheets(sheetNames(4)).Select
+    Worksheets(sheetNames(4)).Rows("1:1").Select
     Selection.Font.Bold = True
     Selection.RowHeight = 15
     Selection.ColumnWidth = 20
