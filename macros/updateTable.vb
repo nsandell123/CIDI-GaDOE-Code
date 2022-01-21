9a3d217a58ecfcbe1a227a1ee910d07f28e60368
Neel Sandell <sandellfamily2013@gmail.com>
2022-01-21 06:42:25 -0500

    Update updateTable.vb

--- macros/updateTable.vb
+++ macros/updateTable.vb
@@ -5,8 +5,8 @@ Sub UpdateTable()
     
     Dim oldDate As String
     oldDate = Cells(LRowFirst, 11).Offset(-6, 0)
-    Cells(LRowFirst, 11).Offset(-5, 0).Value = oldDate
-    Cells(LRowFirst, 11).Offset(-10, 0).Value = oldDate
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-5, 0).Value = oldDate
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-10, 0).Value = oldDate
     
     
     
@@ -19,28 +19,28 @@ Sub UpdateTable()
     fetchedDate = Left(fetchedDate, 4)
     fetchedDate = Left(fetchedDate, 2) & "/" & Right(fetchedDate, 2) & " Totals"
     
-    Cells(LRowFirst, 11).Offset(-6, 0).Value = fetchedDate
-    Cells(LRowFirst, 11).Offset(-6, 0).NumberFormat = "m/d"
-    Cells(LRowFirst, 11).Offset(-11, 0).Value = fetchedDate
-    Cells(LRowFirst, 11).Offset(-11, 0).NumberFormat = "m/d"
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-6, 0).Value = fetchedDate
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-6, 0).NumberFormat = "m/d"
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-11, 0).Value = fetchedDate
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-11, 0).NumberFormat = "m/d"
     
     
     'Actually Copy the Values
-    Cells(LRowFirst, 11).Offset(-5, 1) = Cells(LRowFirst, 11).Offset(-6, 1)
-    Cells(LRowFirst, 11).Offset(-5, 2) = Cells(LRowFirst, 11).Offset(-6, 2)
-    Cells(LRowFirst, 11).Offset(-5, 3) = Cells(LRowFirst, 11).Offset(-6, 3)
-    Cells(LRowFirst, 11).Offset(-5, 4) = Cells(LRowFirst, 11).Offset(-6, 4)
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-5, 1) = Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-6, 1)
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-5, 2) = Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-6, 2)
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-5, 3) = Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-6, 3)
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-5, 4) = Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-6, 4)
     
     
-    Cells(LRowFirst, 11).Offset(-10, 1) = Cells(LRowFirst, 11).Offset(-6, 1)
-    Cells(LRowFirst, 11).Offset(-10, 2) = Cells(LRowFirst, 11).Offset(-6, 2)
-    Cells(LRowFirst, 11).Offset(-10, 3) = Cells(LRowFirst, 11).Offset(-6, 3)
-    Cells(LRowFirst, 11).Offset(-10, 4) = Cells(LRowFirst, 11).Offset(-6, 4)
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-10, 1) = Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-6, 1)
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-10, 2) = Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-6, 2)
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-10, 3) = Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-6, 3)
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-10, 4) = Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-6, 4)
     
-    Cells(LRowFirst, 11).Offset(-6, 1) = Cells(LRowFirst, 11).Offset(-11, 1)
-    Cells(LRowFirst, 11).Offset(-6, 2) = Cells(LRowFirst, 11).Offset(-11, 2)
-    Cells(LRowFirst, 11).Offset(-6, 3) = Cells(LRowFirst, 11).Offset(-11, 3)
-    Cells(LRowFirst, 11).Offset(-6, 4) = Cells(LRowFirst, 11).Offset(-11, 4)
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-6, 1) = Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-11, 1)
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-6, 2) = Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-11, 2)
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-6, 3) = Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-11, 3)
+    Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-6, 4) = Worksheets(sheetNames(1)).Cells(LRowFirst, 11).Offset(-11, 4)
     
     
     
