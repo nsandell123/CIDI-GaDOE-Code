a657da03b3ecbe7ad8e597e0211667cc98a637dc
Neel Sandell <sandellfamily2013@gmail.com>
2022-05-19 21:34:16 -0400

    Updated global checking macro with conditional checking

--- macros/GlobalChecking.vb
+++ macros/GlobalChecking.vb
@@ -1,12 +1,11 @@
-Option Explicit 
-Global sheetNames(12) As String
-Global mainWorkbook As Workbook
 Sub GlobalChecking()
     Set mainWorkbook = ActiveWorkbook
+    If mainWorkbook.Sheets.Count <> 12 Then
+        MsgBox "There should be 12 sheets in the workbook"
+    End If
     Dim i As Long
     For i = 1 To 12
         sheetNames(i) = mainWorkbook.Sheets(i).Name
     Next i
-
 End Sub
 
