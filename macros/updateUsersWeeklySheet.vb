cbd8fc5ea27786a7349385127fb22c159183a18f
Neel Sandell <sandellfamily2013@gmail.com>
2022-05-22 01:26:35 -0400

    Create updateUsersWeeklySheet.vb

---
+++ macros/updateUsersWeeklySheet.vb
@@ -0,0 +1,9 @@
+Sub updateUsersWeeklySheet()
+    Dim LastRowColumnA As Integer
+    LastRowColumnA = Worksheets(8).Cells(Rows.Count, "A").End(xlUp).Row
+    Dim updateAnchorCell As Range
+    Set updateAnchorCell = Worksheets(8).Cells(LastRowColumnA + 1, "A")
+    updateAnchorCell.Value = inputDate
+    updateAnchorCell.Offset(0, 1).Value = updateAnchorCell.Offset(-1, 1).Value + numberNewUsers
+    MsgBox "Finished Updating Users Weekly Sheet"
+End Sub
