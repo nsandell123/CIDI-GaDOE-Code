7b1555672fdce2caeb4b97e73772ec0b583c154c
Neel Sandell <sandellfamily2013@gmail.com>
2022-05-30 07:03:09 -0400

    Update workbook_open_macro.vb

--- macros/workbook_open_macro.vb
+++ macros/workbook_open_macro.vb
@@ -1,3 +1,3 @@
 Private Sub Workbook_Open()
-    MsgBox "To see macros, navigate to cell X295"
+    MsgBox "To see macros, navigate to cell X295" + vbNewLine + "Before using the macros, this week's Users CSV and Requests CSV from the Tools for Life database must be inserted into the workbook" + vbNewLine + "Please rename the first sheet of the workbook (ex: Combined Data 052322 to Combined Data 053022)"
 End Sub
