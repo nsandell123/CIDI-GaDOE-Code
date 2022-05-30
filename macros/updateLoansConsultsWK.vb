b262d915ce0f346df2797e1bf4a438f53b53397c
Neel Sandell <sandellfamily2013@gmail.com>
2022-05-30 06:45:30 -0400

    fixed date formatting

--- macros/updateLoansConsultsWK.vb
+++ macros/updateLoansConsultsWK.vb
@@ -12,11 +12,11 @@ Sub updateLoansConsultsWK()
     Dim lastRowColumnT As Integer
     lastRowColumnT = Worksheets(12).Cells(Rows.Count, "T").End(xlUp).Row
     Set updateAnchorCell = Worksheets(12).Cells(lastRowColumnT + 1, "T")
-    updateAnchorCell.Value = inputDate
+    updateAnchorCell.Value = Left(inputDate, Len(inputDate) - 4)
     updateAnchorCell.Offset(0, 1).Value = updateAnchorCell.Offset(-1, 1).Value + totalConsults
     Worksheets(12).Range(updateAnchorCell, updateAnchorCell.Offset(0, 1)).Borders.LineStyle = xlContinuous
     Worksheets(12).Range(updateAnchorCell, updateAnchorCell.Offset(0, 1)).Borders.Weight = xlThin
-    
+
     MsgBox "Finished Updating Loans & Consults Weekly Sheet"
     
     
