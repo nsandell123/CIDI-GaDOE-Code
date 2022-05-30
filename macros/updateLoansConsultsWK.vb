f768bd0be7f9a059536e50a135099a173a6bbbc0
Neel Sandell <sandellfamily2013@gmail.com>
2022-05-30 06:49:58 -0400

    finished updating date formats

--- macros/updateLoansConsultsWK.vb
+++ macros/updateLoansConsultsWK.vb
@@ -3,7 +3,7 @@ Sub updateLoansConsultsWK()
     LastRowColumnA = Worksheets(12).Cells(Rows.Count, "A").End(xlUp).Row
     Dim updateAnchorCell As Range
     Set updateAnchorCell = Worksheets(12).Cells(LastRowColumnA + 1, "A")
-    updateAnchorCell.Value = inputDate
+    updateAnchorCell.Value = Left(inputDate, Len(inputDate) - 5) + " Totals "
     updateAnchorCell.Offset(0, 1).Value = updateAnchorCell.Offset(-1, 1).Value + totalLoans
     Worksheets(12).Range(updateAnchorCell, updateAnchorCell.Offset(0, 1)).Borders.LineStyle = xlContinuous
     Worksheets(12).Range(updateAnchorCell, updateAnchorCell.Offset(0, 1)).Borders.Weight = xlThin
@@ -12,7 +12,7 @@ Sub updateLoansConsultsWK()
     Dim lastRowColumnT As Integer
     lastRowColumnT = Worksheets(12).Cells(Rows.Count, "T").End(xlUp).Row
     Set updateAnchorCell = Worksheets(12).Cells(lastRowColumnT + 1, "T")
-    updateAnchorCell.Value = Left(inputDate, Len(inputDate) - 4)
+    updateAnchorCell.Value = Left(inputDate, Len(inputDate) - 5) + " Totals "
     updateAnchorCell.Offset(0, 1).Value = updateAnchorCell.Offset(-1, 1).Value + totalConsults
     Worksheets(12).Range(updateAnchorCell, updateAnchorCell.Offset(0, 1)).Borders.LineStyle = xlContinuous
     Worksheets(12).Range(updateAnchorCell, updateAnchorCell.Offset(0, 1)).Borders.Weight = xlThin
